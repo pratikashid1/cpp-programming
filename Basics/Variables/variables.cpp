12d4c6dbf1ebf20664eac34b9fa62035424d2b12
Pratik Kashid <kashidpratik100@gmail.com>
2026-08-30 07:24:25 +0530

    Day 2 - Variables

---
+++ Basics/Variables/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    // Normal Variable
+    int num = 5; // Data_Type Variable_Name = Value
+
+    const int a = 10; // Constant Value can't change
+
+    return 0;
+}
