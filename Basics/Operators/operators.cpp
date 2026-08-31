6b0055ea4749d690674a2547688bd01599903cc5
Pratik Kashid <kashidpratik100@gmail.com>
2026-08-31 07:40:18 +0530

    Day 3 - Operarors mistakes

--- Basics/Operators/operators.cpp
+++ Basics/Operators/operators.cpp
@@ -6,10 +6,10 @@ int main() {
 
     // Arithmetic Operators
     std::cout << "a + b = " << (a + b) << std::endl; // Addition
-    std::cout << "a - b = " << (a + b) << std::endl; // Subtraction
-    std::cout << "a * b = " << (a + b) << std::endl; // Multiplication
-    std::cout << "a / b = " << (a + b) << std::endl; // Division
-    std::cout << "a % b = " << (a + b) << std::endl; // Modulo
+    std::cout << "a - b = " << (a - b) << std::endl; // Subtraction
+    std::cout << "a * b = " << (a * b) << std::endl; // Multiplication
+    std::cout << "a / b = " << (a / b) << std::endl; // Division
+    std::cout << "a % b = " << (a % b) << std::endl; // Modulo
 
     // Unary Operators
     std::cout << "a++ = " << (a++) << std::endl; // Increment
@@ -17,11 +17,11 @@ int main() {
 
     // Relational Operator
     std::cout << "a == b = " << (a == b) << std::endl; // Equal
-    std::cout << "a > b = " << (a == b) << std::endl; // Greater than
-    std::cout << "a >= b = " << (a == b) << std::endl; // Greater than Equal to
-    std::cout << "a < b = " << (a == b) << std::endl; // Lesser than 
-    std::cout << "a <= b = " << (a == b) << std::endl; // Lesser than Equal to
-    std::cout << "a != b = " << (a == b) << std::endl; // Not Equal to
+    std::cout << "a > b = " << (a > b) << std::endl; // Greater than
+    std::cout << "a >= b = " << (a >= b) << std::endl; // Greater than Equal to
+    std::cout << "a < b = " << (a < b) << std::endl; // Lesser than 
+    std::cout << "a <= b = " << (a <= b) << std::endl; // Lesser than Equal to
+    std::cout << "a != b = " << (a != b) << std::endl; // Not Equal to
 
     // Logocal Operators
     std::cout << "a && b = " << (a && b) << std::endl; // AND
@@ -37,10 +37,10 @@ int main() {
     std::cout << "~(a) = " << ~(a) << std::endl; // One's Compliment
 
     // Assignment Operators
-    std::cout << "a += b = " << (a == b) << std::endl; // Add And Assign
-    std::cout << "a -= b = " << (a == b) << std::endl; // Subtract and Assign
-    std::cout << "a /= b = " << (a == b) << std::endl; // devide and subtract
-    std::cout << "a *= b = " << (a == b) << std::endl; // mulriply and subtract
+    std::cout << "a += b = " << (a += b) << std::endl; // Add And Assign
+    std::cout << "a -= b = " << (a -= b) << std::endl; // Subtract and Assign
+    std::cout << "a /= b = " << (a /= b) << std::endl; // devide and subtract
+    std::cout << "a *= b = " << (a *= b) << std::endl; // mulriply and subtract
 
     // Ternary OPerators
     int result = (a > b) ? a : b;
